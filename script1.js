6f3df172bdb605dec0a783d1ced6469a0e621d90
Tushar Tyagi <tushartyagi2110@gmail.com>
2025-06-07 15:29:32 +0530

    Updated UI and scripts, removed 1.mp3, added songs folder

--- script1.js
+++ script1.js
@@ -1,10 +1,15 @@
 console.log("welcome to spotify");
 let songindex=0;
-let audioElement= new Audio('1.mp3');
+let audioElement= new Audio('songs/1.mp3');
 let masterplay=document.getElementById('masterplay');
 let myProgressBar=document.getElementById('myProgressBar');
 let songs=[
-    {songName:"jhoome-jo-pathan"}
+     { songName: "Jhoome Jo Pathan", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
+    { songName: "Let Me Love You", filePath: "songs/2.mp3", coverPath: "covers/2.jpg" },
+    { songName: "Lal Pari", filePath: "songs/4.mp3", coverPath: "covers/4.jpg" },
+    { songName: "Kesariya", filePath: "songs/5.mp3", coverPath: "covers/5.jpg" },
+    { songName: "Tum Hi Ho", filePath: "songs/6.mp3", coverPath: "covers/6.jpg" },
+    { songName: "Natu Natu", filePath: "songs/7.mp3", coverPath: "covers/7.jpg" }
 ]
 
 masterplay.addEventListener('click',()=>{
@@ -32,4 +37,33 @@ myProgressBar.addEventListener('change',()=>{
     audioElement.currentTime = myProgressBar.value*audioElement.duration/100;
 }
 )
+let next=document.getElementById('next');
+next.addEventListener('click',()=>{
+    if(songindex>=songs.length-1){
+       songindex=0;
+    }
+    else{
+        songindex++;
+    }
+    audioElement.src=songs[songindex].filePath;
+    audioElement.currentTime=0;
+    audioElement.play();
+    masterplay.classList.remove('fa-play-circle');
+    masterplay.classList.add('fa-pause-circle');
+})
+let back=document.getElementById('back');
+back.addEventListener('click',()=>{
+    if(songindex<=0){
+        songindex=songs.length-1;
+    }
+    else{
+        songindex--;
+    }
+    audioElement.src=songs[songindex].filePath;
+    audioElement.currentTime=0;
+    audioElement.play();
+    masterplay.classList.remove('fa-play-circle');
+    masterplay.classList.add('fa-pause-circle');
+})
+let icon=document.getElementsByClassName('songplayitem');
 
